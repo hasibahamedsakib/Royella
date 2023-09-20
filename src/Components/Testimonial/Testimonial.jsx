c1ba2374e853d03289c6bdb3982a577917c5c68e
Hasib Ahamed Sakib <hasibahamedsakib1@gmail.com>
2023-09-20 16:06:14 +0600

    created testimonial section

---
+++ src/Components/Testimonial/Testimonial.jsx
@@ -0,0 +1,9 @@
+const Testimonial = () => {
+  return (
+    <section className="bg-[url('/images/home-1/testi-bg.jpg')] bg-[rgba(30,30,30,0.4)] bg-opacity-40 grid items-center justify-center">
+      <div className="Container py-[120px]">{/* section title */}</div>
+    </section>
+  );
+};
+
+export default Testimonial;
